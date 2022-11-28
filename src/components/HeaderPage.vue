b1860543df1d56b325a678e1199df79cefa0b4d1
millooo94 <camillomusmeciunict@gmail.com>
2022-11-28 16:23:06 +0100

    add loader

--- src/components/HeaderPage.vue
+++ src/components/HeaderPage.vue
@@ -1,8 +1,14 @@
 <template>
   <header class="px-3 py-4">
-    <a href="" class="d-flex align-items-center h-100">
-      <img class=""
-      src="../assets/spotify-logo-png-7078.png" alt="logo spotify">
+    <a
+      href=""
+      class="d-flex align-items-center h-100"
+    >
+      <img
+        class=""
+        src="../assets/spotify-logo-png-7078.png"
+        alt="logo spotify"
+      >
     </a>
   </header>
 </template>
--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -1,11 +1,22 @@
 <template>
   <main>
     <div class="container">
-      <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-5 g-5">
+      <div
+        v-if="arrAlbums"
+        class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-5 g-5"
+      >
         <DiskCard
           v-for="album in arrAlbums"
           :key="album.title"
           :album="album"
+        />
+      </div>
+      <div
+        v-else
+        class="loader-container"
+      >
+        <span
+          class="loader"
         />
       </div>
     </div>
@@ -28,10 +39,12 @@ export default {
     };
   },
   created() {
-    axios.get(this.albumApi)
-      .then((axiosResponse) => {
-        this.arrAlbums = axiosResponse.data.response;
-      });
+    setTimeout(() => {
+      axios.get(this.albumApi)
+        .then((axiosResponse) => {
+          this.arrAlbums = axiosResponse.data.response;
+        });
+    }, 6000);
   },
 };
 </script>
@@ -43,5 +56,30 @@ main {
 .container {
   padding: 4rem;
 }
+.loader-container {
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.loader {
+    width: 80px;
+    height: 80px;
+    border: 5px solid #FFF;
+    border-bottom-color: transparent;
+    border-radius: 50%;
+    display: inline-block;
+    box-sizing: border-box;
+    animation: rotation 1s linear infinite;
+    }
+
+    @keyframes rotation {
+    0% {
+        transform: rotate(0deg);
+    }
+    100% {
+        transform: rotate(360deg);
+    }
+    }
 
 </style>
